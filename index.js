9f7224df367d06dd012c306965c9b02fbaf6f1be
hydrogen-studio <hydrogen.studio.llc@gmail.com>
2022-08-26 20:39:08 -0700

    Added UI, backend is WIP

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    title: "Simple Social",
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
